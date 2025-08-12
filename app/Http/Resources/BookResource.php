76e06a29eb0b5f1ba5aaeafd1ee75100cfca49b5
TPH Sharing <thetpainghtut.sharing@gmail.com>
2025-08-12 22:33:49 +0630

    api with resource to create transformation layer

---
+++ app/Http/Resources/BookResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BookResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        // return parent::toArray($request);
+        return [
+            'book_ID' => $this->id,
+            'book_name' => $this->name,
+            'book_price' => $this->price,
+            'photo' => url('/').'/'.$this->image, // URL to the image
+            'short_description' => $this->description,
+            'category_name' => $this->category->name,
+            'author_name' => $this->author->name,
+        ];
+    }
+}
